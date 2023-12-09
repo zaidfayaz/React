9463da36d98cfd739fa6436429fa2bc517d6a7da
zaidfayaz <zaidfayaz1@gmail.com>
2023-12-09 19:08:14 +0530

    modified

--- react_practice/src/App.jsx
+++ react_practice/src/App.jsx
@@ -28,7 +28,7 @@ function App() {
       </button>
       </div>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Edit <code>src/App.jsx</code> and save to test HMR s
         </p>
       </div>
       <p className="read-the-docs">
