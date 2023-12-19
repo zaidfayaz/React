5a1346c1e3fea57539ecedc19d9876348ecbcd82
zaidfayaz <zaidfayaz1@gmail.com>
2023-12-19 12:38:51 +0530

    new file

--- react_practice/src/App.jsx
+++ react_practice/src/App.jsx
@@ -2,51 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Greet from './components/Functional_Component'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='App'>
+        <Greet></Greet>
       </div>
-      <h1>Vite + React </h1>
-      <h2>Hello Zaid, Welcome to React! </h2>
-      <ul>
-          <li>Menu</li>
-          <li>Career</li>
-          <li>About</li>
-          <li>Portfolio</li>
-          <li>Contact Us</li>
-      </ul>
-
-      <div>
-        <h3>This is a great start for the react!</h3>
-        <p>Best of luck</p>
-        
-      </div>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <div>
-        <button onClick={() => setCount((count) => count + 1)}>
-        index is {count}
-      </button>
-      </div>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR s
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more........
-      </p>
+   
     </>
   )
 }
---
+++ react_practice/src/components/Functional_Component.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+// export default function Greet(){
+//     return( <div>
+//         <h1>Hello Zaid Fayaz</h1>
+//     </div>
+//     );
+// }
+//------------------one way-------------------------------
+// function Greet(){
+//     return <h1>Hello Zaid</h1>
+// }
+// export default Greet
+
+//------------------2nd way-------------------------------
+const Greet = () => <h1>Hello Zaid Fayaz! We welcome you in the React Development </h1>
+
+ export default Greet
+
+
