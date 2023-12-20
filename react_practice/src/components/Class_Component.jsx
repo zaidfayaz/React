1b8e959fa01609941f191fbddc4df8a09a8767b4
zaidfayaz <zaidfayaz1@gmail.com>
2023-12-20 16:21:54 +0530

    modified

--- react_practice/src/components/Class_Component.jsx
+++ react_practice/src/components/Class_Component.jsx
@@ -1,7 +1,7 @@
 import React, {Component} from "react";
 class Welcome extends Component {
     render(){
-        return <h1> Hey {this.props.name} - I am a Class Component -a.k.a {this.props.heroName}</h1>
+        return <h1> Hey {this.props.name} - I am a Class Component! - a.k.a {this.props.heroName}</h1>
     }
 }
 export default Welcome
