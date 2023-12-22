9c3af2f8f71a536f0a5d136a14f41036eca19dcc
zaidfayaz <zaidfayaz1@gmail.com>
2023-12-22 16:06:37 +0530

    modified:

--- react_practice/src/components/ChildComponent.jsx
+++ react_practice/src/components/ChildComponent.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 function ChildComponent(props) {
   return (
     <div>
-      <button onClick={()=> props.greetHandler('child')}>Greet Parent</button>
+      <button onClick={()=> props.greetHandler('Child')}>Greet Parent</button>
     </div>
   )
 }
