7331979f4032edf9d563014a3507386fb56960bf
zaidfayaz <zaidfayaz1@gmail.com>
2023-12-23 13:40:57 +0530

    modified

--- react_practice/src/components/ListRendering.jsx
+++ react_practice/src/components/ListRendering.jsx
@@ -1,33 +1,27 @@
 import React from 'react'
 
 function ListRendering() {
-    const names = ['Zaid', 'Iqu', 'Faizan']
+   // const names = ['Zaid', 'Iqu', 'Faizan']
     const persons = [{
-        Id: 1,
-        Name: 'Zaid',
-        Age: 27,
-        Skill: 'FrontEnd',
+        id: 1,
+        name: 'Zaid',
+        age: 27,
+        skill: 'FrontEnd',
     },
     {
-        Id: 2,
-        Name: 'Iqu',
-        Age: 28,
-        Skill: 'React',
+        id: 2,
+        name: 'Iqu',
+        age: 28,
+        skill: 'React',
     },
     { 
-        Id: 3,
-        Name: 'Haseeb',
-        Age: 31,
-        Skill: 'Fullstack',
+        id: 3,
+        name: 'Haseeb',
+        age: 31,
+        skill: 'Fullstack',
     }]
-    const personList = persons.map(person => <h2>IMy name is{person.name}</h2>)
-    return (
-        <div>
-            {
-                names.map(name => <h2>{name}</h2>)
-            }
-        </div>
-    )
+    const personList = persons.map(person => <h2>My name is {person.name}. I am {person.age} years old. I know {person.skill}</h2>)
+    return <div> {personList} </div>
 }
 
 export default ListRendering
