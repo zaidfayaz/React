06a401c5da0e012fc68f85fb273fd9190687ae49
zaidfayaz <zaidfayaz1@gmail.com>
2023-12-26 16:23:19 +0530

    modified

--- react_practice/src/components/Form.jsx
+++ react_practice/src/components/Form.jsx
@@ -1,13 +1,47 @@
 import React, { Component } from 'react'
 
- class Form extends Component {
-  render() {
-    return (
-      <div>
-        Form Component
-      </div>
-    )
-  }
+class Form extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            username: '',
+            comments: ''
+        }
+    }
+    handleUsernameChange = event => {
+        this.setState({
+            username: event.target.value
+        })
+    }
+    handleCommentsChange = event => {
+        this.setState({
+            comments: event.target.value
+        })
+    }
+
+
+    render() {
+        return (
+            <form>
+                <div>
+                    <label>User Name</label>
+                    <input type='text'
+                        value={this.state.username}
+                        onChange={this.handleUsernameChange}>
+                    </input>
+                </div>
+
+                <div>
+                    <label>Comments</label>
+                    <textarea
+                        value={this.state.comments}
+                        onChange={this.handleCommentsChange}>
+                    </textarea>
+                </div>
+            </form>
+        )
+    }
 }
 
 export default Form
