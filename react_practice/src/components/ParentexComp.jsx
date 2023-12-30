e6582766515baa8e3e5cccbb167e455064eb98a8
zaidfayaz <zaidfayaz1@gmail.com>
2023-12-30 15:07:32 +0530

    new file

--- react_practice/src/components/ParentexComp.jsx
+++ react_practice/src/components/ParentexComp.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import PureComp from './PureComp'
 import RegularComp from './RegularComp'
+import MemoComp from './MemoComp'
 
 class ParentexComp extends Component {
 
@@ -22,6 +23,7 @@ render() {
     return (
         <div>
              Parent Component
+             <MemoComp></MemoComp>
             <PureComp name={this.state.name}></PureComp>
             <RegularComp name={this.state.name} ></RegularComp>
       </div>
