1118a13c39f93791740ea489d21a43ed49c62bef
zaidfayaz <zaidfayaz1@gmail.com>
2024-01-04 14:37:42 +0530

    modified

---
+++ react_practice/src/components/RefsDemo.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+ class RefsDemo extends Component {
+  render() {
+    return (
+      <div>
+        <input type='text'></input>
+      </div>
+    )
+  }
+}
+
+export default RefsDemo
