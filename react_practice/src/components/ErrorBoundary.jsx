85cb222d1fc78195ffb4381083cc5052b4202d85
zaidfayaz <zaidfayaz1@gmail.com>
2024-01-10 16:35:19 +0530

    modified

--- react_practice/src/components/ErrorBoundary.jsx
+++ react_practice/src/components/ErrorBoundary.jsx
@@ -14,6 +14,11 @@ import React, { Component } from 'react'
             hasError : true
         }
     }
+    componentDidCatch(error, info){
+        console.log(error);
+        console.log(info);
+
+    }
   render() {
     if (this.state.hasError){
     return <h1>Something Went Wrong</h1>
