7942c79c9c5fb6ed59f3e4a603535f4d676ea347
zaidfayaz <zaidfayaz1@gmail.com>
2024-01-10 16:22:57 +0530

    new file added

---
+++ react_practice/src/components/ErrorBoundary.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+ class ErrorBoundary extends Component {
+    static getDerivedStateFromError(error){
+        
+    }
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default ErrorBoundary
